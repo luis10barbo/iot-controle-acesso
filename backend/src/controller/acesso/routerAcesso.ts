4e466009d5b68360fb81cc51e3f723a59f16225b
Luis Eduardo <luis10barbo2020@gmail.com>
2024-10-17 14:06:44 -0300

    Adicionado codigo de confirmacao de acesso

--- backend/src/controller/acesso/routerAcesso.ts
+++ backend/src/controller/acesso/routerAcesso.ts
@@ -1,12 +1,41 @@
 import { Router } from "express";
+import db from "../../utils/prisma";
+import HttpStatusCode from "../../utils/httpStatusCodes";
 
 const routerAcesso = Router();
-routerAcesso.get("/confirmar", (req, res) => {
+routerAcesso.post("/confirmar", async (req, res) => {
     const idCartao = req.query["id_cartao"];
     const idPorta = req.query["id_porta"];
     const dataInteracao = req.query["data_interacao"];
-    // colocar codigo de confirmar acesso aqui
-    const confirmacaoAcesso = true;
+
+    // validar se foram enviados as informacoes acima
+    if (!idCartao || typeof idCartao != "string") {
+        res.status(HttpStatusCode.BAD_REQUEST).send("id cartao nao definido.")
+        return;
+    }
+    if (!idPorta || typeof idPorta != "string") {
+        res.status(HttpStatusCode.BAD_REQUEST).send("id porta nao definido.")
+        return;
+    }
+
+    // codigo de confirmar acesso aqui
+    let confirmacaoAcesso = false;
+
+    const cartao = await db.cartao.findUnique({ where: { id: idCartao } })
+    if (!cartao) {
+        // cartao nao existe, registrar
+        const cartao = await db.cartao.create({ data: { id: idCartao } });
+        res.status(HttpStatusCode.UNAUTHORIZED).send(confirmacaoAcesso);
+        return;
+    }
+
+    const cartaoAutorizado = await db.cartao.findUnique({ where: { id: idCartao, PortasPermitidas: { some: { id: idPorta } } } })
+    if (!cartaoAutorizado) {
+        // se nao achar nenhum cartao com id idCartao e com PortaPermitida da idPorta passada
+        res.status(HttpStatusCode.UNAUTHORIZED).send(confirmacaoAcesso);
+        return;
+    }
+    confirmacaoAcesso = true;
     res.status(200).send(confirmacaoAcesso);
 })
 
