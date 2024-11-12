29557c2e945c04eeff8d8778963cd784aaeb95af
luis10barbo <luis10barbo2020@gmail.com>
2024-11-12 10:40:30 -0300

    Adicionada rota para autorizar cartao

--- backend/src/controller/acesso/routerCartao.ts
+++ backend/src/controller/acesso/routerCartao.ts
@@ -5,7 +5,64 @@ import HttpStatusCode from "../../utils/httpStatusCodes";
 const routerCartao = Router();
 
 
-routerCartao.post("/cartao", async (req, res) => {
+routerCartao.post("/autorizacao", async (req, res) => {
+    const {id_cartao, autorizacao} = req.body;
+    if (!id_cartao || typeof id_cartao !== "string") {
+        res.status(HttpStatusCode.BAD_REQUEST).send("ID do cartão é obrigatorio.");
+        return
+    }
+
+    if (!autorizacao) {
+        res.status(HttpStatusCode.BAD_REQUEST).send("Valor de autorizacao (booleano) e obrigatorio");
+        return
+    }
+
+    let autorizacaoBool = false; 
+    try {
+        autorizacaoBool = JSON.parse(autorizacao);
+        if (typeof autorizacaoBool !== "boolean") {
+            throw(new Error("Valor de autorizacao nao e um booleano."))
+        }
+    } catch (e) {
+        res.status(HttpStatusCode.BAD_REQUEST).send("Valor de autorizacao nao e um booleano.");
+        return
+    }
+    const porta = await db.porta.findFirst();
+    if (!porta) {
+        res.status(HttpStatusCode.INTERNAL_SERVER_ERROR).send("Nao existe nenhuma porta no sistema.");
+        return
+    }
+    if (autorizacaoBool === true) {
+        db.cartao.update({
+            data: {
+                PortasPermitidas: {
+                    connect: {
+                        id: porta?.id,
+                    }
+                }
+            }, 
+            where: {
+                id: id_cartao
+            }
+        })
+    } else {
+        db.cartao.update({
+            data: {
+                PortasPermitidas: {
+                    disconnect: {
+                        id: porta?.id,
+                    }
+                }
+            }, 
+            where: {
+                id: id_cartao
+            }
+        })
+    }
+    
+})
+
+routerCartao.post("/criar", async (req, res) => {
     const { id } = req.body; 
 
 
